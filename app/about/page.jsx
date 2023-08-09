d0eddb11b7fd9a460900371be2741987c5782bf4
harsh_raut <harshraut73@gmail.com>
2023-08-09 16:30:08 +0530

    first commit

---
+++ app/about/page.jsx
@@ -0,0 +1,16 @@
+
+export const metadata={
+    title:'About Harsh Raut'
+}
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph.</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ app/about/team/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Team</h1>
+      <p>
+      A team is a group of individuals working together to achieve their goal. As defined by Professor Leigh Thompson of the Kellogg School of Management, "[a] team is a group of people who are interdependent ...
+      </p>
+    </div>
+  )
+}
+
+export default TeamPage
